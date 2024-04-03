b303eb43b77bdf108dd90b679aca1805d7ec540b
Ukudot <giovannipanico314@gmail.com>
2024-04-03 09:29:26 +0200

    added conf files

--- keycloak/tools/init.sh
+++ keycloak/tools/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#/opt/keycloak/bin/kc.sh import --file /opt/keycloak/backup/backup.json
-/opt/keycloak/bin/kc.sh start-dev --features scripts --db postgres --db-url-host postgres --db-username $DB_USER --db-password $DB_PASSWORD
+/opt/keycloak/bin/kc.sh start-dev --features scripts --db postgres --db-url-host postgres --db-username $DB_USER --db-password $DB_PASSWORD --import-realm
+#/opt/keycloak/bin/kc.sh import --file /opt/keycloak/backup/backup.json --override false
